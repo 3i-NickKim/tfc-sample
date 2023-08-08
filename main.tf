15a315a9bb20a947541f57c881951703237545c3
3i-NickKim <nick.kim@3i.ai>
2023-08-08 16:04:16 +0900

    Sample

--- main.tf
+++ main.tf
@@ -1,31 +1,52 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
+# provider "aws" {
+#   region = var.region
+# }
+#
+# data "aws_ami" "ubuntu" {
+#   most_recent = true
+#
+#   filter {
+#     name   = "name"
+#     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+#   }
+#
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
+#
+#   owners = ["099720109477"] # Canonical
+# }
+#
+# resource "aws_instance" "ubuntu" {
+#   ami           = data.aws_ami.ubuntu.id
+#   instance_type = var.instance_type
+#
+#   tags = {
+#     Name = var.instance_name
+#   }
+# }
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = var.instance_name
+    Name = "dev"
   }
 }
