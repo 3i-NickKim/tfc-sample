72f56bb43e48de6f2453dd47ac389240d5c66a80
3i-NickKim <nick.kim@3i.ai>
2023-08-08 17:42:00 +0900

    Add team tag

--- main.tf
+++ main.tf
@@ -1,44 +1,3 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-#
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "~> 3.0"
-#     }
-#   }
-# }
-#
-# provider "aws" {
-#   region = var.region
-# }
-#
-# data "aws_ami" "ubuntu" {
-#   most_recent = true
-#
-#   filter {
-#     name   = "name"
-#     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-#   }
-#
-#   filter {
-#     name   = "virtualization-type"
-#     values = ["hvm"]
-#   }
-#
-#   owners = ["099720109477"] # Canonical
-# }
-#
-# resource "aws_instance" "ubuntu" {
-#   ami           = data.aws_ami.ubuntu.id
-#   instance_type = var.instance_type
-#
-#   tags = {
-#     Name = var.instance_name
-#   }
-# }
-
 terraform {
   required_providers {
     aws = {
@@ -57,6 +16,7 @@ resource "aws_vpc" "main" {
 
   tags = {
     Name = "dev"
+    Team = "bd"
   }
 }
 
